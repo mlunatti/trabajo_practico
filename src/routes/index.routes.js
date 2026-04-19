b4771f15d4f8cce7209aada56bf7b262a2379787
mlunatti <mlunatti@egg.gba.gov.ar>
2026-04-19 19:32:03 -0300

    Crear y consultar meidcos pacientes  tratamientos especialidaddes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,14 +3,23 @@
 const { Router } = require('express')
 
 const usuarioRoutes = require('./usuario.routes')
+const pacienteRoutes = require('./paciente.routes')
+const medicoRoutes = require('./medico.routes')
+const tratamientoRoutes = require('./tratamiento.routes')
+const especialidadRoutes = require('./especialidad.routes')
+
 
  const rutas_init = () =>{
         const router = Router()
+//        const router1 = Router()
 
         router.use("/usuarios", usuarioRoutes)
+        router.use("/paciente", pacienteRoutes)
+        router.use("/medico", medicoRoutes)
+        router.use("/tratamiento", tratamientoRoutes)
+        router.use("/especialidad", especialidadRoutes)
 
         return router
-
  }
 
  module.exports={ rutas_init}
